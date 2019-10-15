2659fecc46ce177d1d6740af61abe25383bc254a
TaetaeKim <ty980407@gmail.com>
2019-10-15 17:37:38 +0900

    CSS & HTML Changed

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .models import Post
 def post_list(request):  # request 변수 - 사용자의 웹 주소가 다 넘어가는 변수
     # views.post_list 함수는 이제 DB에서 필요한 데이터를 가져와서
     # post_list.html에게 넘겨줘야 함.
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     # 게시일자가 지금보다 이전으로 들어있는 행만 검색 + 최신순으로 정렬
     # render 함수 화면을 그리는 걸 말함(렌더링)
