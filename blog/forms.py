27e5ed186a9c423719f296456deeecd09a7650f9
Taeyeon Kim <ty980407@gmail.com>
2019-11-05 12:48:38 +0900

    2019.11.05 wk10

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms  # 폼을 작성하기 위하여
+from .models import Post  # Post 모델을 import
+
+
+class PostForm(forms.ModelForm):  # ModelForm을 상속받아서 작성
+    class Meta:  # Meta 클래스를 폼 클래스 내부에 정의
+        model = Post  # 폼을 작성할 모델을 지정
+        fields = ('title', 'text',)  # 양식에서 입력 필요한 항목만 지정
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+# 링크에서 작성하면 정순, 아래에서 지정한 name으로 찾아가는게 역순
 urlpatterns = [
     # path('url', 사용할 함수 혹은 클래스, 이름)
     path('', views.post_list, name='post_list'),  # 나중에 template에서 view로 접근할 때 사용할 이름 지정
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
 ]
